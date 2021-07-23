32a96f0882e3def055f34caf7631eb00fd8d02d6
bobycat22 <anthonyznj@gmail.com>
2021-07-23 00:47:26 -0400

    add icon and email form

--- README.md
+++ README.md
@@ -12,7 +12,9 @@ Monty CSC also hosts events including MontyHacks, and prepares students for the 
 To join, fill out this google form: [interest form](https://forms.gle/HvYdk998iZ3sim5y7)
 
 Here's our linktree: [linktr.ee/montycompsci](https://linktr.ee/montycompsci)
+
 Follow us on instagram: [@montycompsci](https://www.instagram.com/montycompsci)
+
 Find us on facebook: [montycompsci](https://youtu.be/dQw4w9WgXcQ)
 
 Hope to see you in school!
--- index.js
+++ index.js
@@ -3,16 +3,12 @@ nav = document.querySelector('nav');
 mobileBtnExit = document.getElementById('mobile-exit');
 
 mobileBtn.addEventListener('click', () => {
-	// nav.style.display = 'block';
-	// mobileBtn.style.display = 'none';
 	nav.classList.toggle('menu-btn');
 	console.log(nav.classList)
 	console.log(nav.style)
 })
 
 mobileBtnExit.addEventListener('click', () => {
-	// nav.style.display = 'none';
-	// mobileBtn.style.display = 'block';
 	nav.classList.toggle('menu-btn');
 	console.log(nav.classList)
 	console.log(nav.style)
